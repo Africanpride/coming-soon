3b26974ba68a32b18650d4093575ed62a5af4612
pius <pius.opoku@pakatron.com>
2022-08-29 23:18:57 +0000

    all done ...

---
+++ dist/index.5ad81dc4.js
@@ -0,0 +1,2 @@
+"dark"===localStorage.theme||!("theme"in localStorage)&&window.matchMedia("(prefers-color-scheme: dark)").matches?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark"),localStorage.theme="light",localStorage.theme="dark",localStorage.removeItem("theme");
+//# sourceMappingURL=index.5ad81dc4.js.map
